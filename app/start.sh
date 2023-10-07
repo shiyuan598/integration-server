b6fc041de0342945ea4de147df687af40175b495
wangshiyuan <wangshiyuan@zhito.com>
2023-10-07 15:48:03 +0800

    update dockerfile

--- app/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -f schedule_task* ;
-# 检查文件是否存在
-if [ $? -eq 0 ]; then
-  echo "文件已删除";
-fi
-python3.10 -m gunicorn -c gunicorn.conf app:app
--- app/start_prod.sh
+++ app/start_prod.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+# 设置环境变量
+export FLASK_ENV=production
+
+# 1.执行 Flask 数据库迁移
+python3.10 -m flask db upgrade
+# 等待上一步完成
+wait
+
+# 2.删除任务锁文件
 rm -f schedule_task* ;
 # 检查文件是否存在
 if [ $? -eq 0 ]; then
   echo "文件已删除";
 fi
+
+# 3.运行程序
 python3.10 -m gunicorn -c gunicorn.conf app:app
