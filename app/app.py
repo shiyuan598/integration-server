fcde188f1ddab040c5262bc095c66ce28707d706
wangshiyuan <wangshiyuan@zhito.com>
2023-10-09 16:31:45 +0800

    modify max instances

--- app/app.py
+++ app/app.py
@@ -84,7 +84,7 @@ def run_schedule_task():
 
 def init_scheduler():
     print(f"\n\n初始化定时任务\n", flush=True)
-    scheduler = BackgroundScheduler(max_instances=5)
+    scheduler = BackgroundScheduler(max_instances=10)
     scheduler.add_job(run_schedule_task, "interval", seconds=60)
     scheduler.start()
 
